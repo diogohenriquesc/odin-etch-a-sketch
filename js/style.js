a67ed5b4503f63abe51cefa6bcaa9a1816dcf67c
Diogo Henrique <diogohs110@gmail.com>
2023-06-25 18:08:20 -0300

    Added range

--- js/style.js
+++ js/style.js
@@ -1,20 +1,39 @@
 const grid = document.querySelector('.grid');
+const gridRange = document.querySelector('#range-grid');
 
 function createGrid(number) {
   for (let i = 0; i < number; i++) {
     const gridItem = document.createElement('div');
     gridItem.classList.add('grid__item');
+    gridItem.addEventListener('mouseenter', changeColor)
     grid.appendChild(gridItem);
   }
 }
 
-createGrid(256);
+function removeGrid() {
+  const gridItems = document.querySelectorAll('.grid__item');
+  gridItems.forEach(item => item.remove());
+}
 
-
-const gridItems = document.querySelectorAll('.grid__item');
-gridItems.forEach(item => item.addEventListener('mouseenter', changeColor));
+function updateGridLayout(value) {
+  grid.style = `grid-template-columns: repeat(${value}, 1fr)`;
+}
 
 function changeColor(e) {
   e.target.classList.add('hovered');
 }
 
+gridRange.addEventListener('change', (e) => {
+  const rangeValue = e.target.value;
+  updateRangeOutput(rangeValue);
+  removeGrid();
+  createGrid(rangeValue * rangeValue);
+  updateGridLayout(rangeValue);
+});
+
+function updateRangeOutput(value) {
+  const rangerOutput = document.querySelector('.range__output');
+  rangerOutput.textContent = `${value} x ${value}`;
+}
+
+createGrid(256);
